73e2645abdf1c77a0449c941be7a49544c63817f
leonel <leonel.mariano.g@gmail.com>
2026-01-14 15:46:21 -0300

    feat(kanban): Add ability to add new cards

--- kanban-board/js/components/App.js
+++ kanban-board/js/components/App.js
@@ -5,6 +5,7 @@ const App = {
     components: {
         List,
     },
+
     template: `
         <div class="h-screen bg-linear-to-r from-cyan-500 to-blue-500 flex flex-col gap-8">
             <header class="bg-black/20 py-4 shadow-sm">
@@ -14,7 +15,7 @@ const App = {
             <main class="h-full">
                 <div class="h-full px-2 overflow-x-auto">
                     <div id="board" class="grid auto-cols-[20em] grid-flow-col gap-2 items-start h-full">
-                        <List v-for="list in lists" :title="list.title" :cards="list.cards"/>
+                        <List v-for="list in lists" :id="list.id" :title="list.title" :cards="list.cards" @add-card="addNewCard"/>
                         
                         <button class="bg-black/20 py-4 rounded-md shadow-sm cursor-pointer hover:bg-black/10 font-bold text-sm text-black">
                             + Add another list
@@ -28,6 +29,13 @@ const App = {
     data( ) {
         return {
             lists
+        }
+    },
+
+    methods: {
+        addNewCard({ listId, content }) {
+            const list = this.lists.find((list) => list.id === listId);
+            list.cards.push({ id: new Date().getTime(), content, labels: [] });
         }
     }
 }
--- kanban-board/js/components/List.js
+++ kanban-board/js/components/List.js
@@ -2,6 +2,7 @@ import Card from "./Card.js";
 
 const List = {
     components: { Card },
+
     template: `
         <div class="bg-neutral-300 rounded-md shadow-sm">
              <div class="px-4 py-2">
@@ -12,19 +13,73 @@ const List = {
                  <ul class="space-y-2">
                      <li v-for="card in cards">
                         <Card :content="card.content" :labels="card.labels" />
-                     </li>    
+                     </li>
+                     
+                     <li>
+                        <div class="bg-neutral-100 rounded-sm px-2 pb-2 pt-2 shadow-sm space-y-2" v-show="canAddCard">
+                            <form @submit.prevent="handleAddNewCard()">
+                                <textarea name="content" v-model="content" class="px-2 py-1 w-full" />
+                                <div class="flex gap-2 text-sm">
+                                    <button 
+                                        type="submit" 
+                                        class="bg-purple-500 text-white text-sm hover:bg-purple-400 cursor-pointer rounded-md py-1 px-4"
+                                    >
+                                        Add
+                                    </button>
+                                    <button 
+                                        type="reset"
+                                        class="bg-neutral-500 text-white text-sm hover:bg-neutral-400 cursor-pointer rounded-md py-1 px-4"
+                                        @click="closeCreateNewCardForm()"
+                                    >
+                                        Cancel
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </li>    
                  </ul>
     
-                 <button class="font-bold text-sm text-neutral-500 cursor-pointer hover:text-neutral-700">+
-                     Add another card
+                 <button 
+                    class="font-bold text-sm text-neutral-500 cursor-pointer hover:text-neutral-700"
+                    @click="openCreateNewCardForm()"
+                    v-show="!canAddCard"
+                  >
+                    + Add another card
                  </button>
              </div>
         </div>
     `,
 
+    data() {
+        return {
+            content: '',
+            canAddCard: false
+        }
+    },
+
     props: {
+        id: Number,
         title: String,
         cards: Array
+    },
+
+    methods: {
+        openCreateNewCardForm () {
+            this.canAddCard = true;
+        },
+
+        closeCreateNewCardForm () {
+            this.canAddCard = false;
+        },
+
+        handleAddNewCard() {
+            this.$emit('add-card', {
+                listId: this.id,
+                content: this.content
+            });
+            this.canAddCard = !this.canAddCard;
+            this.content = '';
+        }
     }
 }
 
