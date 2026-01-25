71269d93898afa5320926cf82c15c7712c8b87bf
leonel <leonel.mariano.g@gmail.com>
2026-01-25 18:11:02 -0300

    Migrate CustomButton to composition API

--- src/components/CustomButton.vue
+++ src/components/CustomButton.vue
@@ -1,5 +1,5 @@
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script setup lang="ts">
+import { computed } from 'vue';
 
 const BASE_STYLE = 'cursor-pointer disabled:cursor-not-allowed rounded-sm py-2 px-6 text-nowrap';
 
@@ -10,34 +10,30 @@ const VARIANTS = {
   clear: 'bg-black/0 hover:bg-black/20',
 };
 
-export default defineComponent({
-  emits: ['click'],
+const emit = defineEmits<{
+  (e: 'click'): void;
+}>();
 
-  props: {
-    variant: {
-      type: String,
-      default: 'default',
-    },
-    fullWidth: {
-      type: Boolean,
-      default: false,
-    },
+const props = withDefaults(
+  defineProps<{
+    variant: string;
+    fullWidth: boolean;
+  }>(),
+  {
+    variant: 'default',
+    fullWidth: false,
   },
+);
 
-  computed: {
-    classes() {
-      const variant = VARIANTS[this.variant as keyof typeof VARIANTS];
-      const width = this.fullWidth ? 'w-full' : '';
-      return [BASE_STYLE, width, variant];
-    },
-  },
-
-  methods: {
-    onClick() {
-      this.$emit('click');
-    },
-  },
+const classes = computed(() => {
+  const variant = VARIANTS[props.variant as keyof typeof VARIANTS];
+  const width = props.fullWidth ? 'w-full' : '';
+  return [BASE_STYLE, width, variant];
 });
+
+const onClick = () => {
+  emit('click');
+};
 </script>
 
 <template>
