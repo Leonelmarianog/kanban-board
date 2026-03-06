69c719c90ec4920562b95c0910e61fa66b0772b6
leonel <leonel.mariano.g@gmail.com>
2026-03-06 13:29:40 -0300

    feat(frontend): add notification for user when logout fails

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,9 @@
 import './assets/main.css';
+import 'vue-toastification/dist/index.css';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import Toast, { POSITION } from 'vue-toastification';
 
 import App from './App.vue';
 import router from './router';
@@ -12,5 +14,8 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.use(VueQueryPlugin);
+app.use(Toast, {
+  position: POSITION.BOTTOM_RIGHT,
+});
 
 app.mount('#app');
