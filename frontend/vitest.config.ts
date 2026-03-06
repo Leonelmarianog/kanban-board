3a5e973395e9a334f05c6e51782afcdee9e1bcae
leonel <leonel.mariano.g@gmail.com>
2026-03-06 11:16:22 -0300

    tests(frontend): fix missing environment variable when testing

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -7,6 +7,9 @@ export default mergeConfig(
   defineConfig({
     test: {
       environment: 'jsdom',
+      env: {
+        VITE_API_BASE_URL: 'http://localhost:8000/api',
+      },
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
       setupFiles: ['./test/setup.ts'],
