39dcb989153da202bb83ae80670880b31f6b3fed
leonel <leonel.mariano.g@gmail.com>
2026-03-27 22:14:53 -0300

    chore(backend): fix phpstan error for InvalidCredentialsException

--- backend/modules/Application/UseCases/Auth/LoginUser/Exceptions/InvalidCredentialsException.php
+++ backend/modules/Application/UseCases/Auth/LoginUser/Exceptions/InvalidCredentialsException.php
@@ -4,10 +4,10 @@ namespace Modules\Application\UseCases\Auth\LoginUser\Exceptions;
 
 final class InvalidCredentialsException extends LoginUserException
 {
-    public function __construct(
-        string $message = 'Invalid credentials.',
-        int $statusCode = 401
-    ) {
-        parent::__construct($message, $statusCode);
+    public function __construct()
+    {
+        parent::__construct(
+            message: 'Invalid credentials.',
+        );
     }
 }
